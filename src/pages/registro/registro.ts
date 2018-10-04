653b24be2b6eec649efab66dee6077d6fc6bc876
Angelo Guerrero Medina <angelomedinabarraza@gmail.com>
2018-10-03 22:52:53 -0600

    agregando select con ngIf y model y data para cambiar el form dependiendo del tipo de registro

--- src/pages/registro/registro.ts
+++ src/pages/registro/registro.ts
@@ -15,11 +15,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RegistroPage {
 
+
+  userOption : Array<{name: string}>;
+  option     : Array<{name: string}>;
+  
+  estado:any = "null";
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    this.option  = [{ name: "Cliente" },{name: "Restaurante"}]; 
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegistroPage');
+  }
+
+  onItemSelection(selection) {
+
+    if(selection.name == "Cliente"){
+      this.estado="Cliente";
+    }else{
+      this.estado="Restaurante";
+    }
   }
 
 }

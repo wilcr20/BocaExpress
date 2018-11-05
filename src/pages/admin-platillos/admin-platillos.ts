b46f853205263ea2b52c5b2f05f11798fdb15437
Wilfred <wilcr20@gmail.com>
2018-11-04 19:09:03 -0600

    Cambio minimo

--- src/pages/admin-platillos/admin-platillos.ts
+++ src/pages/admin-platillos/admin-platillos.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Platillo } from '../../model/platillo/platillo.model';
+import { PlatilloService } from '../../services/platillo/platillo.service';
+import 'rxjs/add/operator/map'
+import { Observable } from 'rxjs/Observable';
 
 /**
  * Generated class for the AdminPlatillosPage page.
@@ -16,9 +20,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class AdminPlatillosPage {
 
   tabBarElement:any;
+  restaurante:any; // Variable para recibir el json enviado
   mostrar:any=true;
+  dishList: Observable<any[]>
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  //lista de platillos general
+  platillos:any=[];
+  platillosLocal:  any[] = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,public platilloService: PlatilloService) {
+    this.restaurante= this.navParams.get('rest');
+    console.log("recibe : ", this.restaurante);
+    this.getPlatillos();
+    this.allPlatillos();
+
   }
 
   ionViewDidLoad() {
@@ -34,9 +49,49 @@ export class AdminPlatillosPage {
     document.getElementById("TabPrincipal").className="MostrarTab";
   }
 
+
+  getPlatillos(){
+    this.dishList = this.platilloService.getPlatilloList()
+    .snapshotChanges()
+    .map(
+      changes => {
+        return changes.map( c =>({
+          key: c.payload.key, ...c.payload.val()
+        }))
+      }
+    )
+    .map(changes => changes.reverse());
+  }
+
+  allPlatillos(){
+    this.dishList.forEach(platillo => {
+      console.log("Platillo", platillo);
+      this.platillos= platillo;
+   });
+   console.log("lista platillos: ", this.platillos);
+   this.getPlatilloLocal();
+
+  }
+
+  getPlatilloLocal(){
+    console.log("entra a getPlatilloLocal()");
+    console.log("tam :", this.platillos.length);
+
+     for(var i=0; i< this.platillos.length;i++){
+       console.log("-",this.platillos[i]);
+      if(this.platillos[i].idRestaurante == this.restaurante.key){
+        this.platillosLocal.push(this.platillos[i]);
+        console.log("HALLA IGULA");
+      }
+    }
+
+  }
+
   mostrarP(){
+    //console.log(this.platillosLocal.length);
     this.mostrar= true;
   }
+
 
   addP(){
     this.mostrar= false;

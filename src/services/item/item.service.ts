ea8f47b6da57b7e3ecc671e1ef7809a758337da4
Wilfred <wilcr20@gmail.com>
2018-11-18 12:24:25 -0600

    Uniendo....

--- src/services/item/item.service.ts
+++ src/services/item/item.service.ts
@@ -2,22 +2,22 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Item } from '../../model/item/item.model';
 
- 
+
 @Injectable()
 export class ItemService {
- 
+
     private itemListRef = this.db.list<Item>('Item');
- 
+
     constructor(private db: AngularFireDatabase) { }
- 
+
     getItemList() {
         return this.itemListRef;
     }
- 
+
     addItem(item: Item) {
         return this.itemListRef.push(item);
     }
- 
+
     removeItem(key: any) {
         return this.itemListRef.remove(key);
     }
@@ -29,4 +29,8 @@ export class ItemService {
     getItems(idCompra : string){
        return this.db.list('Item', ref => ref.orderByChild('idCompra').equalTo(idCompra)).valueChanges();
     }
+
+    editItem(item,key){
+      this.db.database.ref('Item/'+key).set(item);
+  }
 }
